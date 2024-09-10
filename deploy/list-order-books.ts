4958870032b49f7773588b045188b22e5d03e90f
Rabbo-Babbo <jaredfocose@gmail.com>
2024-09-09 17:05:16 -0700

    Create list-order-books.ts

---
+++ deploy/list-order-books.ts
@@ -0,0 +1,58 @@
+import {
+  createSharkyClient,
+  enabledOrderBooks,
+  OrderBook,
+  SHARKY_PROGRAM_ID,
+} from '@sharkyfi/client'
+import { PublicKey } from '@solana/web3.js'
+import { Command } from 'commander'
+import fetch from 'node-fetch'
+import { createProvider } from './lib/createProvider'
+
+// Usage
+// npx ts-node examples/list-order-books.ts --wallet-path ~/.config/solana/id.json
+// You can get the list of orderbook pubkeys from get-orderbooks.ts
+
+async function main() {
+  const cli = new Command()
+  const options = cli
+    .requiredOption('--wallet-path <path>', 'path to private key file')
+    .parse()
+    .opts()
+
+  const provider = createProvider(options.walletPath)
+  const sharkyClient = createSharkyClient(
+    provider,
+    new PublicKey(SHARKY_PROGRAM_ID),
+    'mainnet'
+  )
+  const { program } = sharkyClient
+
+  // Fetch and log the current orderbooks
+  const orderBooks = await sharkyClient.fetchAllOrderBooks({ program })
+  const collectionNames = await sharkyClient.fetchAllNftLists({ program })
+
+  const nftListPubKeyToNameMap = Object.fromEntries(
+    collectionNames.map(({ pubKey, collectionName }) => [
+      pubKey,
+      collectionName,
+    ])
+  )
+
+  const orderBooksByName = Object.fromEntries(
+    orderBooks
+      .map((ob: OrderBook) => [
+        nftListPubKeyToNameMap[
+          ob.orderBookType.nftList!.listAccount.toString()
+        ],
+        {
+          pubkey: ob.pubKey.toString(),
+          enabled: enabledOrderBooks.includes(ob.pubKey.toString()),
+        },
+      ])
+      .sort((a, b) => (a[0] < b[0] ? -1 : 1))
+  )
+  console.dir({ orderBooksByName })
+}
+
+main()
